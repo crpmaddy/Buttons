12264b2b522c7ccf2f9c874167545624782b0383
Andrew S. Morrison <asm@etsy.com>
2010-12-18 13:58:20 -0500

    moving less to require

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-require(['http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js',
+require(['js/vendor/less-1.0.35.min.js',
+             'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js',
              'http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.min.js',
              'http://github.com/jquery/jquery-metadata/raw/master/jquery.metadata.js',
              'js/jquery.asmform.js',
              'js/jquery.log.js'], function() {
     require.ready(function() {
         $('#asmform').asmform();
+        less.env = "development";
+        less.watch();
     });
 });

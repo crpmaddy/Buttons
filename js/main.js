648325eb3eadff327cf111602ff5929f79c6783a
Andrew Morrison <asm@etsy.com>
2011-06-01 22:19:44 -0400

    Adding html5shiv from http://code.google.com/p/html5shiv/

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ require(['js/vendor/less-1.0.35.min.js',
            'js/vendor/jquery.ba-resize.min.js',
            'js/jquery.asmform.js',
            'js/jquery.shrinktext.js',
-           'js/jquery.log.js'], function() {
+           'js/jquery.log.js',
+	   'js/vendor/html5.js'], function() {
     require.ready(function() {
         $('#asmform').asmform();
 
